bbafaf6e959c352e06c7a9d261abbbe7bfbd67d2
louisdelp <73433096+louisdelp@users.noreply.github.com>
2020-11-21 15:56:01 +0100

    Add files via upload

--- dsfba_project/Untitled.R
+++ dsfba_project/Untitled.R
@@ -1,13 +1,39 @@
-crime <- data2 %>% select(`Incident Date`,`Incident Time`,`Incident Year`, `Incident Day of Week`,
-                 `Incident Category`,`Incident Subcategory`, `Incident Description`,
+crime <- data2 %>% select(`Incident Date`,`Incident Time`, `Incident Day of Week`,
+                 `Incident Category`, `Incident Description`,
                  `Police District`, point)
+#select the variable that we're interested in (time horizon 2018-2020)
+
+
+
+#######################################################################################
+#BIND DES DEUX DATA ->2003-2018 & 2018-2020 POUR AVOIR 2003-2020
+data1_colchange <- data1 %>% mutate(Date = mdy(Date)) %>% 
+  rename(
+    `Incident Date` = Date,
+    `Incident Time` = Time,
+    `Incident Day of Week` = DayOfWeek,
+    `Incident Category` = Category,
+    `Incident Description` = Descript,
+    `Police District` = PdDistrict,
+    point = location
+  ) #change the column name of the data set with horizon 2003-2018 in order to bind the 2 data set
+
+crime2 <- data1_colchange %>% select(`Incident Date`,`Incident Time`, `Incident Day of Week`,
+                                     `Incident Category`, `Incident Description`,
+                                     `Police District`, point)
 #select the variable that we're interested in
 
-View(crime %>% count(`Incident Category`, name = "num") %>% arrange(desc(num)))
+police2003_2020 <- bind_rows(crime, crime2)
+#bind both data set
+
+
+police2003_2020 %>% count(`Incident Category`, name = "num") %>% arrange(desc(num))
 #count the number of crime given their category (time period 2018-2020)
-View(crime %>% count(`Incident Day of Week`, name = "day") %>% arrange(desc(day)))
+police2003_2020 %>% count(`Incident Day of Week`, name = "day") %>% arrange(desc(day))
 #count the number of crime given their day of the week (time period 2018-2020)
 
+#######################################################################################
+#MANIPULATION SUR LA DATA SET 2018-2020
 
 crime_number <- crime %>% arrange(`Incident Time`) %>%  arrange(`Incident Date`) %>%
   add_column(crime = 1) %>% select(crime, `Incident Date`)
@@ -30,7 +56,8 @@ crime_clean <- crime_number %>%
   separate( `Incident Date`, into = c("year","mon", "day"), sep = "[-]+")
 # change the incident date column in order to have year, month and the day in different column
 
-#################################################
+#######################################################################################
+#MANIPULATION SUR LA DATA SET 2003-2018
 
 library(lubridate)
 
@@ -51,10 +78,39 @@ crime_number_monhtly2 <- crime_number2 %>%
 #group the number of crime per month given the time period 2003 to 2017
 
 ggplot(crime_number_monhtly2, aes(x = monthcrime, y = tcrimemonth)) +
-  geom_smooth()
+  geom_smooth(se = F)
 #timeseries graph
 
-#################################################
+
+#######################################################################################
+#MANIPULATION SUR LA DATA SET 2003-2020
+
+crime_number0320 <- police2003_2020 %>% arrange(`Incident Time`) %>%  arrange(`Incident Date`) %>%
+  add_column(crime = 1) %>% select(crime, `Incident Date`)
+#establish the number of crime from 2018 to 2020
+
+crime_number_monhtly0320 <- crime_number0320 %>%
+  mutate(month = month(`Incident Date`, label = TRUE),
+         year  = year(`Incident Date`)) %>%
+  group_by(year, month) %>%
+  summarise(tcrimemonth = sum(crime)) %>% mutate(monthcrime = make_date(year, month)) %>%
+  select(monthcrime, tcrimemonth)
+#group the number of crime per month given the time period 2018 to 2020
+
+ggplot(crime_number_monhtly0320, aes(x = monthcrime, y = tcrimemonth)) +
+  geom_smooth(se = F) +
+#timeseries graph
+
+
+crime_clean <- crime_number %>%
+  separate( `Incident Date`, into = c("year","mon", "day"), sep = "[-]+")
+# change the incident date column in order to have year, month and the day in different column
+
+#######################################################################################
+#######################################################################################
+
+
+#VISUALISATION
 
 library("leaflet")
 library("magrittr")
@@ -64,7 +120,4 @@ SFMAP <-
   addProviderTiles(providers$Stamen.TonerLines,
                    options = providerTileOptions(opacity = 1)) %>%
   addProviderTiles(providers$Stamen.TonerLabels)
-
-
-
 
