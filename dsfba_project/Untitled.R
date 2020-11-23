244117cdae4005d89e9b1bc5791a34b4808bd38a
louisdelp <73433096+louisdelp@users.noreply.github.com>
2020-11-23 18:26:18 +0100

    Add files via upload

--- dsfba_project/Untitled.R
+++ dsfba_project/Untitled.R
@@ -4,14 +4,17 @@ crime <- data2 %>% select(`Incident Date`,`Incident Time`, `Incident Day of Week
 #select the variable that we're interested in (time horizon 2018-2020)
 
 crime_separated <-
-  crime %>% # extract from database
-  separate(point, into = c("long", "lat"), sep = "[,]+")
- crime_separated$lat = substr(crime_separated$lat, 2 , nchar(crime_separated$lat) - 1)
-  crime_separated$long = substr(crime_separated$long, 2 , nchar(crime_separated$long))
-    
+  crime %>% filter(!is.na(point)) %>% separate(point, into = c("lon", "lat"), sep = "[,]+")
+ 
+crime_separated$lat = substr(crime_separated$lat, 2 , nchar(crime_separated$lat) - 1)
+  crime_separated$lon = substr(crime_separated$lon, 2 , nchar(crime_separated$lon))
+  options(digits = 13)
+  crime_separated$lat <- sapply( crime_separated$lat, as.numeric)
+  crime_separated$lon <- sapply( crime_separated$lon, as.numeric)
+
 View(crime_separated)
  View(crime)
-
+ 
 #######################################################################################
 #BIND DES DEUX DATA ->2003-2018 & 2018-2020 POUR AVOIR 2003-2020
 data1_colchange <- data1 %>% mutate(Date = mdy(Date)) %>% 
@@ -32,7 +35,6 @@ crime2 <- data1_colchange %>% select(`Incident Date`,`Incident Time`, `Incident 
 
 police2003_2020 <- bind_rows(crime, crime2)
 #bind both data set
-
 
 police2003_2020 %>% count(`Incident Category`, name = "num") %>% arrange(desc(num))
 #count the number of crime given their category (time period 2018-2020)
@@ -73,7 +75,6 @@ data1.2 <- data1 %>% mutate(Date = mdy(Date))
 crime_number2 <- data1.2  %>% arrange(Time) %>%  arrange(Date) %>%
   add_column(crime = 1) %>% select(crime, Date)
 #establish the number of crime from 2003 to 2017
-
 
 
 crime_number_monhtly2 <- crime_number2 %>%
@@ -126,19 +127,33 @@ crime_clean <- crime_number %>%
 #VISUALISATION
 
 library("leaflet")
-library("magrittr")
+SFMAP <-
+  leaflet() %>% addTiles() %>% #create a variable leaflet on which we add the map Tiles
+  setView(-122.42, 37.78, zoom = 11) %>%   addMarkers(-122.42, 37.78, popup = "SF")
+
+
+###################################################
+#POUR REPRESENTATION AVEC ADD MARKER -> BIEN FILTRER LA DATA SET SINON CA PLANTE CPU PROBLEM
+
+essai <-
+  crime %>% filter(!is.na(point), `Incident Date` == "2020-08-15") %>%
+  separate(point, into = c("lat", "lon"), sep = "[,]+")
+
+essai$lon = substr(essai$lon, 2 , nchar(essai$lon) - 1)
+essai$lat = substr(essai$lat, 2 , nchar(essai$lat))
+options(digits = 13)
+essai$lat <- sapply( essai$lat, as.numeric)
+essai$lon <- sapply( essai$lon, as.numeric)
+
 
 SFMAP <-
-  leaflet() %>% addTiles() %>% setView(-122.42, 37.78, zoom = 11) %>%
+  leaflet() %>% addTiles() %>% #create a variable leaflet on which we add the map Tiles
+  setView(-122.42, 37.78, zoom = 11) %>%
+  addMarkers(lng = essai$lon, lat = essai$lat, popup = essai$`Incident Category`) %>%
   addProviderTiles(providers$MtbMap) %>%
   addProviderTiles(providers$Stamen.TonerLines,
                    options = providerTileOptions(opacity = 1)) %>%
   addProviderTiles(providers$Stamen.TonerLabels)
 
 
-
-leaflet(data = crime_separated) %>%
-  addTiles() %>%
-  addMarkers() %>%
-  frameWidget()
 
