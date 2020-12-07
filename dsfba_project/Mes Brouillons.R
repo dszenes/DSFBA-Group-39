8c04d68d442cee0bc3c09753d50ca916446a59df
dszenes <73389030+dszenes@users.noreply.github.com>
2020-12-07 14:31:10 +0100

    Update Mes Brouillons.R

--- dsfba_project/Mes Brouillons.R
+++ dsfba_project/Mes Brouillons.R
@@ -1,1 +1,10 @@
 fit1 <- step(lm(`Total crimes`~ . -`Analysis Neighborhood` -`Violent crimes`, data = crime_demostats_2016), direction = "forward")
+
+fit2 <- lm(crime_demostats_2016$`Total crimes`~crime_demostats_2016$`Units of Housing` + crime_demostats_2016$`High School or Less`)
+
+fit3 <- lm(crime_demostats_2016$`Total crimes`~crime_demostats_2016$`Non-Family Households`+crime_demostats_2016$`Number of people Poverty`)
+
+
+summary(fit2)
+
+summary(fit3)
