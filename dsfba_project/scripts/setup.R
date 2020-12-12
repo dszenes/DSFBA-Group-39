fcf81fc61f7a727c1da2a8156db798fdb2f98d27
dszenes <73389030+dszenes@users.noreply.github.com>
2020-12-12 13:22:05 +0100

    Update setup.R

--- dsfba_project/scripts/setup.R
+++ dsfba_project/scripts/setup.R
@@ -5,7 +5,7 @@
 # load the required packages
 packages <- c(
   "here", # for the project's organization
-  "tidyverse", "lubridate", # for wrangling
+  "tidyverse", "lubridate", "reshape2", # for wrangling
   "ggrepel", "gghighlight", "patchwork", "maps", "scales", "leaflet", "sf", "sp", "spData", "highcharter" # for plotting
   "knitr", "kableExtra", "bookdown", "rmarkdown" # for the report
 )
